494d344cdc0f566a4a60cf0c7ee7314ecb9f2690
BEUMGOO <captain_kbg@naver.com>
2019-06-23 23:56:23 +0900

    save text file

--- src/compare.js
+++ src/compare.js
@@ -1,5 +1,16 @@
-import diff from "diff";
+const diff = require("diff");
 
-module.exports = function (msg) { 
-  console.log(msg);
+module.exports = text => {
+  const check = filePath => {
+    var isExist = false;
+    try {
+      fs.statSync(filePath);
+      isExist = true;
+    } catch (err) {
+      isExist = false;
+    }
+    return isExist;
+  };
+
+  var diff = diff.createPatch(fileName, oldStr, newStr, oldHeader, newHeader);
 };
--- src/twitter_crawler.js
+++ src/twitter_crawler.js
@@ -2,43 +2,53 @@ const request = require("request");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const compare = require("./compare");
-const upload = require("./aws_s3_upload");
 
-const url = "https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/";
+const url =
+  "https://developer.twitter.com/en/docs/tweets/data-dictionary/overview/";
 
 const title_arr = ["user-object", "tweet-object"];
 
-title_arr.forEach( async (v)=>{
-  await request(url+v, (e, response, body) => {
+title_arr.forEach(async v => {
+  await request(url + v, (e, response, body) => {
     if (e) {
       console.error(e);
     }
     try {
       const $ = cheerio.load(body);
-      const twitter_object = $(
-        ".d02-inline-code-snippet__copy"
-      ).text();
-  
-      // fs.writeFile(
-      //   "./userobject.json",
-      //   JSON.stringify(twitter_object, null, ""),
-      //   err => {
-      //     if (err) throw err;
-      //     console.log("The file has been saved!");
-      //   }
-      // );
-      
-      let uploadCheck = false;
-      let uploadLoop = 0;
-      while (!uploadCheck || uploadLoop <= 3) {
-        uploadLoop++;
-        uploadCheck = upload(twitter_object);
-      }
 
+      $(".main-content")
+        .children(".d05")
+        .remove();
+
+      $(".main-content")
+        .children(".d06-anchor-link-menu")
+        .remove();
+
+      let twitter_object = $(".main-content")
+        .text()
+        .replace(/^\n/gm, "");
+
+      //compare();
+      var dt = new Date();
+      console.log(Date(dt.getFullYear()));
+      const path = dt.getFullYear();
+
+      //ディレクトリの確認後、保存
+      if (!fs.existsSync(`.test/${path}`)) {
+        console.log("ディレクトリ生成");
+        fs.mkdirSync(`.test/${path}`);
+      }
+      fs.writeFileSync(`.test/${path}/test02.txt`, twitter_object);
+
+      // S3保留
+      // let uploadCheck = false;
+      // let uploadLoop = 0;
+      // while (!uploadCheck || uploadLoop <= 3) {
+      //   uploadLoop++;
+      //   uploadCheck = upload(twitter_object);
+      // }
     } catch (e) {
       console.error(e);
     }
   });
-})
-
-
+});
